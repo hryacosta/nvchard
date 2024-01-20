1001b24e46803a0ea73edbca689bf404a7804286
Henry Acosta <henry@mftech.io>
2024-01-20 16:31:39 -0500

    core: updates

--- configs/conform.lua
+++ configs/conform.lua
@@ -21,6 +21,19 @@ local options = {
   -- instead of the above code you could just use a loop! the config is just a table after all!
 
   format_on_save = function(bufnr)
+    local ignore_filetypes = {}
+    if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+      return
+    end
+    -- Disable with a global or buffer-local variable
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    -- Disable autoformat for files in a certain path
+    local bufname = vim.api.nvim_buf_get_name(bufnr)
+    if bufname:match "/node_modules/" then
+      return
+    end
     return {
       timeout_ms = 500,
       lsp_fallback = true,
